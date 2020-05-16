9d6694313ad11241b4144984f5b15e5867558ef8
Kenimu <55797264+Kenimu@users.noreply.github.com>
2020-05-16 23:36:36 +0900

    5月16日

--- ITP1/ITP1_8_C/Main.java
+++ ITP1/ITP1_8_C/Main.java
@@ -1,26 +1,26 @@
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
 
-        String str;
         int[] count = new int[26];
-        char[] ch = null;
-        System.out.println(count.length);
         while(sc.hasNextLine()){
-            str = sc.nextLine();
-            ch = str.toLowerCase().toCharArray();
-            
-            for(int i=0;i<ch.length;i++){
-                if(ch[i] >= 'a' && ch[i] <= 'z'){
-                    count[ch[i] - 'a']++;
+            String str = sc.nextLine().toLowerCase();
+            for(int i=0;i<str.length();i++){
+                char ch = str.charAt(i);
+                if('a' <= ch && ch <= 'z'){
+                    count[ch - 'a']++;
                 }
             }
         }
+
         for(int i=0;i<count.length;i++){
-            System.out.println((char)(i + 'a') + " : " + count[i]);
+            out.println((char)(i + 'a') + " : " + count[i]);
         }
+
+        out.flush();
     }
 }
